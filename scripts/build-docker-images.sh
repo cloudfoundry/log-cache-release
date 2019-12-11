715622c748fe926ae0c32ea4b08fc2d28759c949
Tom Chen <tochen@pivotal.io>
2019-12-11 11:09:00 -0700

    Add containerization artifacts
    [#170083776]
    
    Signed-off-by: Ben Fuller <bfuller@pivotal.io>

---
+++ scripts/build-docker-images.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+
+docker build -t logcache/log-cache:latest ${SCRIPT_ROOT}/src -f ${SCRIPT_ROOT}/src/cmd/log-cache/Dockerfile
+docker build -t logcache/syslog-server:latest ${SCRIPT_ROOT}/src -f ${SCRIPT_ROOT}/src/cmd/syslog-server/Dockerfile
+docker build -t logcache/log-cache-gateway:latest ${SCRIPT_ROOT}/src -f ${SCRIPT_ROOT}/src/cmd/gateway/Dockerfile
