1191b539505158a2561978c2c3994ec0361d9ed5
Ben Fuller <benjaminf@vmware.com>
2022-04-11 17:07:07 +0000

    update metric registry library

--- src/go.mod
+++ src/go.mod
@@ -8,19 +8,17 @@ require (
 	code.cloudfoundry.org/go-envstruct v1.5.0
 	code.cloudfoundry.org/go-log-cache v1.0.1-0.20211123204853-93b7f0836b81
 	code.cloudfoundry.org/go-loggregator/v8 v8.0.2-0.20200722201844-b5130958b65d
-	code.cloudfoundry.org/go-metric-registry v0.0.0-20200413202920-40d97c8804ec
-	code.cloudfoundry.org/tlsconfig v0.0.0-20200131000646-bbe0f8da39b3
+	code.cloudfoundry.org/go-metric-registry v0.0.0-20220408214155-d9c3faa1e9e3
+	code.cloudfoundry.org/tlsconfig v0.0.0-20211123175040-23cc9f05b6b3
 	github.com/Benjamintf1/unmarshalledmatchers v0.0.0-20190408201839-bb1c1f34eaea
 	github.com/OneOfOne/xxhash v1.2.8 // indirect
 	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
-	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
 	github.com/benbjohnson/jmphash v0.0.0-20141216154655-2d58f234cd86
 	github.com/benjamintf1/unmarshalledmatchers v0.0.0-20190408201839-bb1c1f34eaea // indirect
 	github.com/cespare/xxhash v1.1.0
 	github.com/cloudfoundry/gosigar v1.2.0
 	github.com/dvsekhvalnov/jose2go v0.0.0-20180829124132-7f401d37b68a
 	github.com/emirpasic/gods v1.12.0
-	github.com/go-logfmt/logfmt v0.5.0 // indirect
 	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/gorilla/mux v1.7.4
@@ -28,15 +26,15 @@ require (
 	github.com/influxdata/go-syslog/v3 v3.0.1-0.20201006113939-efe45e3cff92
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
-	github.com/onsi/ginkgo v1.13.0
-	github.com/onsi/gomega v1.10.1
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus/client_golang v1.7.0
+	github.com/onsi/ginkgo v1.16.5
+	github.com/onsi/gomega v1.19.0
+	github.com/prometheus/client_golang v1.12.1
 	github.com/prometheus/client_model v0.2.0
-	github.com/prometheus/common v0.10.0
+	github.com/prometheus/common v0.33.0
 	github.com/prometheus/prometheus v2.13.1+incompatible // pinned
 	github.com/shirou/gopsutil v2.20.5+incompatible
-	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f
+	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
 	google.golang.org/grpc v1.40.0
-	google.golang.org/protobuf v1.27.1
+	google.golang.org/protobuf v1.28.0
 )
--- src/vendor/code.cloudfoundry.org/go-metric-registry/testhelpers/spy_registry.go
+++ src/vendor/code.cloudfoundry.org/go-metric-registry/testhelpers/spy_registry.go
@@ -10,14 +10,21 @@ import (
 )
 
 type SpyMetricsRegistry struct {
-	mu      sync.Mutex
-	Metrics map[string]*SpyMetric
+	mu           sync.Mutex
+	Metrics      map[string]*SpyMetric
+	debugMetrics bool
 }
 
 func NewMetricsRegistry() *SpyMetricsRegistry {
 	return &SpyMetricsRegistry{
 		Metrics: make(map[string]*SpyMetric),
 	}
+}
+
+func (s *SpyMetricsRegistry) RegisterDebugMetrics() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.debugMetrics = true
 }
 
 func (s *SpyMetricsRegistry) NewCounter(name, helpText string, opts ...metrics.MetricOption) metrics.Counter {
@@ -45,9 +52,25 @@ func (s *SpyMetricsRegistry) NewHistogram(name, helpText string, buckets []float
 	defer s.mu.Unlock()
 
 	m := newSpyMetric(name, helpText, opts)
+	m.buckets = buckets
 	m = s.addMetric(m)
 
 	return m
+}
+
+func (p *SpyMetricsRegistry) RemoveGauge(g metrics.Gauge) {
+	sm := g.(*SpyMetric)
+	p.removeMetric(sm)
+}
+
+func (p *SpyMetricsRegistry) RemoveHistogram(h metrics.Histogram) {
+	sm := h.(*SpyMetric)
+	p.removeMetric(sm)
+}
+
+func (p *SpyMetricsRegistry) RemoveCounter(c metrics.Counter) {
+	sm := c.(*SpyMetric)
+	p.removeMetric(sm)
 }
 
 func (s *SpyMetricsRegistry) addMetric(sm *SpyMetric) *SpyMetric {
@@ -59,6 +82,18 @@ func (s *SpyMetricsRegistry) addMetric(sm *SpyMetric) *SpyMetric {
 	}
 
 	return s.Metrics[n]
+}
+
+func (s *SpyMetricsRegistry) removeMetric(sm *SpyMetric) {
+	n := getMetricName(sm.name, sm.Opts.ConstLabels)
+
+	delete(s.Metrics, n)
+}
+
+func (s *SpyMetricsRegistry) GetDebugMetricsEnabled() bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.debugMetrics
 }
 
 func (s *SpyMetricsRegistry) GetMetric(name string, tags map[string]string) *SpyMetric {
@@ -99,7 +134,7 @@ func (s *SpyMetricsRegistry) HasMetric(name string, tags map[string]string) bool
 
 func newSpyMetric(name, helpText string, opts []metrics.MetricOption) *SpyMetric {
 	sm := &SpyMetric{
-		name: name,
+		name:     name,
 		helpText: helpText,
 		Opts: &prometheus.Opts{
 			ConstLabels: make(prometheus.Labels),
@@ -110,7 +145,7 @@ func newSpyMetric(name, helpText string, opts []metrics.MetricOption) *SpyMetric
 		o(sm.Opts)
 	}
 
-	for k, _ := range sm.Opts.ConstLabels {
+	for k := range sm.Opts.ConstLabels {
 		sm.keys = append(sm.keys, k)
 	}
 	sort.Strings(sm.keys)
@@ -119,13 +154,14 @@ func newSpyMetric(name, helpText string, opts []metrics.MetricOption) *SpyMetric
 }
 
 type SpyMetric struct {
-	mu    sync.Mutex
-	value float64
-	name  string
+	mu       sync.Mutex
+	value    float64
+	buckets  []float64
+	name     string
 	helpText string
 
-	keys     []string
-	Opts     *prometheus.Opts
+	keys []string
+	Opts *prometheus.Opts
 }
 
 func (s *SpyMetric) Set(c float64) {
@@ -156,6 +192,12 @@ func (s *SpyMetric) HelpText() string {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	return s.helpText
+}
+
+func (s *SpyMetric) Buckets() []float64 {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.buckets
 }
 
 func getMetricName(name string, tags map[string]string) string {
