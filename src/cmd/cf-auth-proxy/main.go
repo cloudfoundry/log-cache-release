358fba32925b045fb8601f04699e485d158226ff
Ben Fuller <benjaminf@vmware.com>
2022-04-15 17:52:34 +0000

    incorrect implementation of pprof will hang server and fail functionality
    - determined error when working on metrics-discovery-release

--- src/cmd/cf-auth-proxy/main.go
+++ src/cmd/cf-auth-proxy/main.go
@@ -62,7 +62,7 @@ func main() {
 	if cfg.MetricsServer.DebugMetrics {
 		metrics.RegisterDebugMetrics()
 		pprofServer := &http.Server{Addr: fmt.Sprintf("127.0.0.1:%d", cfg.MetricsServer.PprofPort), Handler: http.DefaultServeMux}
-		go loggr.Println("PPROF SERVER STOPPED " + pprofServer.ListenAndServe().Error())
+		go func() { loggr.Println("PPROF SERVER STOPPED " + pprofServer.ListenAndServe().Error()) }()
 	}
 
 	var options []auth.UAAOption
